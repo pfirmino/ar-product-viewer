3b81d356cc44dcc8e36f2543dd680046b2178813
pfirmino <pedrofirmino@hotmail.com>
2020-10-22 20:44:19 -0300

    HTTPS redirect implemented

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
+  .use(requireHTTPS)
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/', (req, res) => {
+    res.render('pages/index');
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+  function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+      return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+  }
