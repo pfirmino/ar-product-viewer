3b94e00ee9511d57f57e9a6a624b6f1f8bd06fb3
pfirmino <pedrofirmino@hotmail.com>
2020-10-23 15:56:18 -0300

    Updated to use Aframe Framework

--- index.js
+++ index.js
@@ -3,19 +3,12 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(requireHTTPS)
   .use(express.static(path.join(__dirname, 'public')))
+  .use('/aframe', express.static(path.join(__dirname, '/node_modules/aframe/dist/')))
+  .use('/aframe-extras', express.static(path.join(__dirname, '/node_modules/aframe-extras/dist/')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => {
     res.render('pages/index');
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
-
-  function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
-      return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-  }
