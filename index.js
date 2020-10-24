63440640e27c985ad9b8c3a4e05b2f0cc33acc67
pfirmino <pedrofirmino@hotmail.com>
2020-10-24 20:00:15 -0300

    Changed aframe dependencies

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .use('/aframe', express.static(path.join(__dirname, '/node_modules/aframe/dist/')))
-  .use('/aframe-extras', express.static(path.join(__dirname, '/node_modules/aframe-extras/dist/')))
-  .use('/aframe-ar', express.static(path.join(__dirname, '/node_modules/aframe-ar/dist/')))
   .use("/", express.static(__dirname + '/index.html'))
   .get('/', (req, res) => {
       })
